0c369227e80c9c6cb5242b67ead1b159437b0146
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2022-09-27 17:22:34 +0530

    Registration 1 page complete

--- app/src/main/java/com/example/tog_s/Register.java
+++ app/src/main/java/com/example/tog_s/Register.java
@@ -2,7 +2,10 @@ package com.example.tog_s;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button registerBtn = (Button) findViewById(R.id.registerbtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(),RegisterSecond.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
